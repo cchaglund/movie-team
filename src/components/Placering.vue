b0238c05684937c43d4f8ccec71651ff65e1134f
Christofer Haglund <christofer.haglund@outlook.com>
2019-01-16 11:39:18 +0100

    placering och seat komponenter

---
+++ src/components/Placering.vue
@@ -0,0 +1,97 @@
+<template>
+	<div>
+		<div class="container">
+			<div 
+				class="seats-row"
+				v-for="(seatsPerRow, index) in this.salonger[0].seatsPerRow">
+
+				<Seat
+					v-for="index in seatsPerRow"
+					:id="setID"
+				/>
+					
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+	import Seat from '@/components/Seat.vue'
+
+	export default {
+		name: 'Placering',
+		props: {
+
+		},
+		components: {
+			Seat
+		},
+		computed: {
+			setID: function() {
+				this.counter = this.counter + 1
+				console.log(this.counter)
+				return this.counter
+			}
+		},
+		data() {
+			return {
+				counter: 0,
+				takenSeats: [12, 3, 24],
+				salonger: [  
+				  {
+				    "name": "Stora Salongen",
+				    "seats": 81,
+				    "seatsPerRow": [
+				      8,
+				      9,
+				      10,
+				      10,
+				      10,
+				      10,
+				      12,
+				      12
+				    ]
+				  },
+				  {
+				    "name": "Lilla Salongen",
+				    "seats": 55,
+				    "seatsPerRow": [
+				      6,
+				      8,
+				      9,
+				      10,
+				      10,
+				      12
+				    ]
+				  }
+				]
+			}
+		},
+		methods: {
+			buildSeatPlan() {
+
+			}
+		}
+	}
+</script>
+
+
+<style>
+
+.container {
+	display: flex;
+	flex-direction: column;
+
+	align-items: center;
+	background-color: lightgray;
+	width: 300px;
+	height: 300px;
+	padding: 0.5rem;
+}
+
+.seats-row {
+	display: flex;
+}
+	
+</style>
---
+++ src/components/Seat.vue
@@ -0,0 +1,56 @@
+<template>
+	<div 
+		class="seat"
+		:class="{'selected': isSelected, 'unselected': !isSelected}"
+		@click="selectSeat()"
+	>
+	{{id}}
+		
+	</div>
+</template>
+
+
+
+
+
+<script>
+
+	export default {
+		name: 'Seat',
+		data() {
+			return {
+				isSelected: false
+			}
+		},
+		props: {
+			id: Number
+		},
+		methods: {
+			selectSeat() {
+				this.isSelected = true
+			}
+		}
+	}
+	
+</script>
+
+
+
+<style>
+	.seat {
+		min-height: 20px;
+		min-width: 19px;
+		background-color: white;
+		margin: 3px;
+		border: 1px solid black;
+		border-radius: 0px 0px 4px 4px;
+	}
+
+	.unselected:hover {
+		background-color: yellow;
+	}
+
+	.selected {
+		background-color: green;
+	}
+</style>
