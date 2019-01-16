e380799190dbb2e4d4509ecb7639c97cee1a9f97
Christofer Haglund <christofer.haglund@outlook.com>
2019-01-16 16:20:36 +0100

    simple nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,13 @@
-
-
 <template>
   <div id="app">
     <div id="nav">
-<div class="button login">
-<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
-  Logga in
-</button>
-<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
-  Registrera dig!
-</button>
-</div>
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img class="img-responsive" style="max-width:100px;" src="../src/images/image1.png"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="app.vue">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="movies.vue">Movies</a>
-      </li>
-     
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Child Movies</a>
-      </li>
-       <li class="nav-item dropdown active">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-   <div class="container h-100">
-      <div class="d-flex justify-content-center h-100">
-        <div class="searchbar">
-          <input class="search_input" type="text" name="" placeholder="Search...">
-          <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
-
-
-
-      
+      <router-link to="/">Home</router-link> |      
+      <router-link to="/filmer">Filmer</router-link> |
+      <router-link to="/barnfilmer">Barnfilmer</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/user">User</router-link> |
+      <router-link to="/user">Confirmation</router-link> |
+      <router-link to="/user">Salonger</router-link> |
     </div>
     <router-view/>
   </div>
@@ -69,92 +20,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #89ACCE;
+  min-height: 100vh;
+  height: 100%;
+}
+
+body {
+  padding: 0;
 }
 
 html {
-  margin-top:-50px;
+  height: 100vh;
 }
-div.button.login {
-  margin-right: 1650px;
-}
-button.btn.btn-primary {
-  margin-right: 10px;
-}
-@media (max-width: 767px){
-  .img-responsive {
-    width:50px;
-  }
-}
-@media (max-width: 767px){
-  div.searchbar {
-    max-width:200px;
-  }
-}
-@media (max-width: 767px){
-  button.btn.btn-primary {
-    margin-right:10px;
-  }
-}
-@media (max-width: 767px){
-  div.button.login {
-    margin-right:150px;
-  }
-}
-<style>
-       body,html{
-    height: 100%;
-    width: 100%;
-    margin: 0;
-    padding: 0;
-    background: #e74c3c !important;
-    }
-
-    .searchbar{
-    margin-bottom: auto;
-    margin-top: auto;
-    height: 60px;
-    background-color: #353b48;
-    border-radius: 30px;
-    padding: 10px;
-    margin-right:-1000px;
-    }
-
-    .search_input{
-    color: white;
-    border: 0;
-    outline: 0;
-    background: none;
-    width: 0;
-    caret-color:transparent;
-    line-height: 40px;
-    transition: width 0.4s linear;
-    }
-
-    .searchbar:hover > .search_input{
-    padding: 0 10px;
-    width: 200px;
-    caret-color:red;
-    transition: width 0.4s linear;
-    }
-
-    .searchbar:hover > .search_icon{
-    background: white;
-    color: #e74c3c;
-    }
-
-    .search_icon{
-    height: 40px;
-    width: 40px;
-    float: right;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    color:white;
-    }
-    </style>
 </style>
-
-
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -9,15 +8,45 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/filmer',
+      name: 'alla-filmer',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import(/* webpackChunkName: "about" */ './views/AllaFilmer.vue')
+    },
+    {
+      path: '/barnfilmer',
+      name: 'barnfilmer',
+      component: () => import(/* webpackChunkName: "about" */ './views/Barnfilmer.vue')
+    },
+    {
+      path: '/bokning',
+      name: 'bokning',
+      component: () => import(/* webpackChunkName: "about" */ './views/Bokning.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "about" */ './views/Login.vue')
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: () => import(/* webpackChunkName: "about" */ './views/User.vue')
+    },
+    {
+      path: '/confirmation',
+      name: 'confirmation',
+      component: () => import(/* webpackChunkName: "about" */ './views/Confirmation.vue')
+    },
+    {
+      path: '/salonger',
+      name: 'salonger',
+      component: () => import(/* webpackChunkName: "about" */ './views/Salonger.vue')
+    },
   ]
 })
