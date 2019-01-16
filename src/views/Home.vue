e18f1381f69afcb2b9f43e2aff84c1a90f7a029c
Hanna <hanna.berg93@gmail.com>
2019-01-16 12:56:59 +0100

    Auto stash before merge of "AllMovies" and "origin/AllMovies"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="movies">
-		<h2>Alla filmer</h2>
-		<AllMovies></AllMovies>
-	</div>
+  <div class="movies">
+    <h2>Alla filmer</h2>
+    <AllMovies></AllMovies>
+  </div>
 </template>
 
 
@@ -13,38 +13,18 @@ import AllMovies from '@/components/AllMovies.vue'
 export default {
   	name: 'home',
   	components: {
-		AllMovies
+  		AllMovies
   }
-}	
+} 
 </script>
 
 
 <style>
-	@import url(https://use.fontawesome.com/releases/v5.5.0/css/all.css);
+  @import url(https://use.fontawesome.com/releases/v5.5.0/css/all.css);
 
-	#app,
-	html{
-		background-color: black;
-		color: white;
-	}
-</style>
-
-
-<!-- <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  #app,
+  html{
+    background-color: black;
+    color: white;
   }
-}
-</script> -->
+</style>
