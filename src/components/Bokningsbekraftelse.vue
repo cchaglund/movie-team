3cfd258c581c75b71b023843290af07db1556315
jahed <vision2025bd@yahoo.com>
2019-01-17 14:54:51 +0100

    updatering

---
+++ src/components/Bokningsbekraftelse.vue
@@ -0,0 +1,141 @@
+ <template>
+  <div class="Bokningsbekraftelse">
+  	<h1> Bokningsbekraftelse</h1>
+		<nav><a href="#hot">Movie-team</a></nav>
+		<section id="hot">
+			<img src="../assets/1.png">
+			<h2>Your purcahse is confirmed</h2>
+			<h3> Cofirmation ID:695857</h3>
+			<section class="colum-layout">
+				<section class="main-column">
+					<div>
+						<p>Cinema Name</p>
+						<p>Hall Name</p>
+						<p>Movie Title</p>
+						<p>Rating</p>
+						<p>Show Date</p>
+						<p>Show time</p>
+						<p>Seat Selected</p>
+						
+					</div>
+				</section>
+				<section class="sidebar-1">
+					<div>
+						<p>GSC QUEEE MALL </p>
+						<p>7</p>
+						<p>KingsMan: The Secret Service </p>
+						<p>18</p>
+						<p>Lördag 17:00</p>
+						<p>17:00</p>
+						<p>C17,C18,B18</p>
+					</div>
+				</section>
+			</section>
+		</section>
+
+</div>
+
+  
+</template>
+
+
+
+
+ 
+
+<script>
+export default {
+  name: 'Bokningsbekraftelse',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: black;
+}
+
+a {
+  display: block;
+  padding: 10px;
+  color: white;
+  font-size: 35px;
+}
+
+nav {
+  
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100vw;
+  text-align: center;
+}
+
+
+#hot {
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  text-align: center;
+}
+
+
+#hot h2 {
+  font-size: 20px;
+  background-color: rgba(132, 31, 31, 0.9);
+  color: white;
+  margin: 10px;
+  padding: 20px 20px 20px 20px;
+}
+#hot h3 {
+  font-size: 20px;
+  background-color: rgba(132, 31, 31, 0.9);
+  color: white;
+  margin: 10px;
+  padding: 20px 20px 20px 20px;
+}
+
+
+.colum-layout {
+  display: flex;
+  background-color: rgba(22, 21, 21, 0.9);
+  color: White;
+  box-sizing: border-box;
+  padding: 20px 35px 20px 35px;
+  min-width: 50%;
+  }
+
+.main-column {
+  flex: 1;
+  text-align: left;
+  flex-wrap: nowrap;
+}
+
+.sidebar-1 {
+  flex: 1;
+  flex-wrap: nowrap;
+  text-align: right;
+}
+
+@media (max-width:600px) {
+  .colum-layout {
+    flex-wrap: nowrap;
+    min-width: 100%;
+    margin-right: 40px;
+  }
+
+}
+</style>
+
+
+
--- src/components/Bokningscomponent.vue
+++ src/components/Bokningscomponent.vue
@@ -1,114 +1,111 @@
 
-
-
 <template>
   <div class="Bookningscomponent">
 
   	<h1> Bokningscomponent</h1>
+	
+<div class="container">
+  <div class="row">
 
 
+    <div class="col">
+
+		<h2>Välj Film</h2>               
+					<div class="dropdown">
+					    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+					      Välj Film
+					    </button>
+						    <div class="dropdown-menu">
+						      <a class="dropdown-item" href="#">Kings man</a>
+						      <a class="dropdown-item" href="#">Spider man</a>
+						      <a class="dropdown-item" href="#">Armagedon</a>
+						      <a class="dropdown-item" href="#">Avengers </a>
+						      <a class="dropdown-item" href="#">The lost city</a>
+						    </div>
+					</div>
+	</div>
 
 
-  	<div class="row">
-    	<div class="col bg-success">
-
-			<div class="container">
-				<h2>Välj Film</h2>               
-				  <div class="dropdown">
-				    	<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-				      Välj Film
-				    	</button>
-					    <div class="dropdown-menu">
-					      <a class="dropdown-item" href="#">Link 1</a>
-					      <a class="dropdown-item" href="#">Link 2</a>
-					      <a class="dropdown-item" href="#">Link 3</a>
-					    </div>
-				  </div>
-			</div>
-		</div>
-
-
-
-
-
-	    <div class="col bg-warning" >
-
-			<div class="container">
-					<h2>Välj biljet</h2>               
+    <div class="col">
+    
+		<h2>Välj biljet</h2>               
 					<div class="dropdown">
 					    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
 					      Välj biljet
 					    </button>
 						    <div class="dropdown-menu">
-						      <a class="dropdown-item" href="#">Link 1</a>
-						      <a class="dropdown-item" href="#">Link 2</a>
-						      <a class="dropdown-item" href="#">Link 3</a>
+						      <a class="dropdown-item" href="#">1</a>
+						      <a class="dropdown-item" href="#">2</a>
+						      <a class="dropdown-item" href="#">3</a>
+						       <a class="dropdown-item" href="#">4</a>
+						      <a class="dropdown-item" href="#">5</a>
+						      <a class="dropdown-item" href="#">6</a>
+						      <a class="dropdown-item" href="#">7</a>
+						      <a class="dropdown-item" href="#">8</a>
+						      <a class="dropdown-item" href="#">9</a>
+						       <a class="dropdown-item" href="#">10</a>
+						      <a class="dropdown-item" href="#">11</a>
+						      <a class="dropdown-item" href="#">12</a>
 						    </div>
 					</div>
-			</div>
-		</div>
+	</div>
 
 
+    <div class="w-100"></div>
 
-	    <div class="col bg-success">
 
-			<div class="container">
-				<h2>Välj plats</h2>               
+    <div class="col">
+
+		<h2>Välj Biograf</h2>               
 					<div class="dropdown">
 					    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-					      Välj plats
+					      Välj Biograf
 					    </button>
 						    <div class="dropdown-menu">
-						      <a class="dropdown-item" href="#">Link 1</a>
-						      <a class="dropdown-item" href="#">Link 2</a>
-						      <a class="dropdown-item" href="#">Link 3</a>
+						      <a class="dropdown-item" href="#">Malmö</a>
+						      <a class="dropdown-item" href="#">Lund</a>
+						      <a class="dropdown-item" href="#">Helsingborg</a>
 						    </div>
 					</div>
-			</div>
-		</div>
+	</div>
 
 
-
-
-		<div class="col bg-warning">
-			<div class="container">
-				<h2>Välj Dag</h2>                
-				<div class="dropdown">
-					<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-						      Välj Dag
-					</button>
+    <div class="col">
+    
+		<h2>Välj Tid</h2>               
+					<div class="dropdown">
+					    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+					      Välj Tid
+					    </button>
 						    <div class="dropdown-menu">
-						      <a class="dropdown-item" href="#">Måndag kl.17:00 </a>
-						        <a class="dropdown-item" href="#">Måndag kl.19:00 </a>
-						          <a class="dropdown-item" href="#">Måndag kl.21:00 </a>
-						      <a class="dropdown-item" href="#">Tisdag kl. 17:00</a>
-						       <a class="dropdown-item" href="#">Tisdag kl. 19:00</a>
-						        <a class="dropdown-item" href="#">Tisdag kl. 21:00</a>
-						      <a class="dropdown-item" href="#">Onsdag kl. 17:00</a>
-						      <a class="dropdown-item" href="#">Onsdag kl. 19:00</a>
-						      <a class="dropdown-item" href="#">Onsdag kl. 21:00</a>
+						      <a class="dropdown-item" href="#">Måndag kl.17:00</a>
+						      <a class="dropdown-item" href="#">Måndag kl.19:00</a>
+						      <a class="dropdown-item" href="#">Måndag kl.21:00</a>
+						      <a class="dropdown-item" href="#">Tisdag kl.17:00</a>
+						       <a class="dropdown-item" href="#">Tisdag kl.19:00</a>
+						       <a class="dropdown-item" href="#">Tisdag kl.21:00</a>
+						      <a class="dropdown-item" href="#">Onsdag kl.17:00</a>
+						      <a class="dropdown-item" href="#">Onsdag kl.19:00</a>
+						      <a class="dropdown-item" href="#">Onsdag kl.21:00</a>
 						      <a class="dropdown-item" href="#">Torsdag kl.17:00</a>
 						      <a class="dropdown-item" href="#">Torsdag kl.19:00</a>
 						      <a class="dropdown-item" href="#">Torsdag kl.21:00</a>
-						      <a class="dropdown-item" href="#">Fredag kl. 17:00</a>
-						      <a class="dropdown-item" href="#">Fredag kl. 19:00</a>
-						      <a class="dropdown-item" href="#">Fredag kl. 21:00</a>
-						      <a class="dropdown-item" href="#">Lördag kl. 13:00 </a>
-						      <a class="dropdown-item" href="#">Lördag kl. 15:00 </a>
-						      <a class="dropdown-item" href="#">Lördag kl. 17:00 </a>
-
-						      <a class="dropdown-item" href="#">Söndag</a>
+						     <a class="dropdown-item" href="#">Fredag kl.17:00</a>
+						     <a class="dropdown-item" href="#">Fredag kl.19:00</a>
+						     <a class="dropdown-item" href="#">Fredag kl.21:00</a>
+						      <a class="dropdown-item" href="#">Lördag kl.13:00</a>
+						       <a class="dropdown-item" href="#">Lördag kl.15:00</a>
+						      <a class="dropdown-item" href="#">Lördag kl.17:00</a>
+						      <a class="dropdown-item" href="#">Lördag kl.19:00</a>
+						      <a class="dropdown-item" href="#">Lördag kl.21:00</a>
+						      <a class="dropdown-item" href="#">Söndag kl.13:00</a>
+						       <a class="dropdown-item" href="#">Söndag kl.15:00</a>
+						      <a class="dropdown-item" href="#">Söndag kl.17:00</a>
+						      <a class="dropdown-item" href="#">Söndag kl.19:00</a>
+						      <a class="dropdown-item" href="#">Söndag kl.21:00</a>
 						    </div>
-				</div>
-			</div>
-		</div>
-    </div>
-
-
-
-
-
-
+					</div>
+	</div>
 
 
 
@@ -116,7 +113,11 @@
 
 
 </div>
-    
+
+
+
+</div>
+</div>
   
 </template>
 
