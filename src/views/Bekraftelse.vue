d0ed825e0f6c8551bc19202556c698602af5d786
jahed <vision2025bd@yahoo.com>
2019-01-17 14:54:51 +0100

    updatering

--- src/views/Bekraftelse.vue
+++ src/views/Bekraftelse.vue
@@ -1,3 +1,21 @@
 <template>
-	
+  <div class="Bekraftelse">
+    <Bokningsbekraftelse />
+  </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+
+import Bokningsbekraftelse from '@/components/Bokningsbekraftelse.vue'
+
+
+export default {
+  name: 'Bekraftelse',
+  components: {
+     Bokningsbekraftelse
+  }
+}
+
+</script>
