f74a0902f7d736c4d1f6f93abdc042c86d84dd68
Alban <kingenalban@gmail.com>
2019-01-17 09:51:38 +0100

    fixat länkarna till navbar

--- src/App.vue
+++ src/App.vue
@@ -3,55 +3,31 @@
 <template>
   <div id="app">
     <div id="nav">
-<div class="button login">
-<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
-  Logga in
-</button>
-<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
-  Registrera dig!
-</button>
-</div>
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img class="img-responsive" style="max-width:100px;" src="../src/images/image1.png"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="app.vue">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="movies.vue">Movies</a>
-      </li>
-     
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Child Movies</a>
-      </li>
-       <li class="nav-item dropdown active">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-   <div class="container h-100">
-      <div class="d-flex justify-content-center h-100">
-        <div class="searchbar">
-          <input class="search_input" type="text" name="" placeholder="Search...">
-          <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
-        </div>
-      </div>
+<div class="nav">
+  <div class="nav-header">
+    <div class="nav-title">
+      <img style="max-width:100px;max-height:50px;" src="../src/images/image1.png"></img>
     </div>
   </div>
+  <div class="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  <input type="checkbox" id="nav-check">
+  <div class="nav-links">
+    <router-link to="home">Home</router-link>
+    <router-link to="movies">Filmsida</router-link>
+    <router-link to="barnfilmer">Barnfilmer</router-link>
+    <router-link to="bokning">Bokning</router-link>
+    <router-link to="inloggning">Inloggning</router-link>
+    <router-link to="anvandare">Användare</router-link>
+    <router-link to="bekraftelse">Bekräftelse</router-link>
+    <router-link to="salonger">Salonger</router-link>
+  </div>
+</div>
 </nav>
 
 
@@ -72,88 +48,103 @@
   margin-top: 60px;
 }
 
-html {
-  margin-top:-50px;
+* {
+  box-sizing: border-box;
 }
-div.button.login {
-  margin-right: 1650px;
+
+body {
+  margin: 0px;
+  font-family: 'segoe ui';
 }
-button.btn.btn-primary {
-  margin-right: 10px;
+
+.nav {
+  height: 50px;
+  width: 100%;
+  background-color: #4d4d4d;
+  position: relative;
 }
-@media (max-width: 767px){
-  .img-responsive {
-    width:50px;
+
+.nav > .nav-header {
+  display: inline;
+}
+
+.nav > .nav-header > .nav-title {
+  display: inline-block;
+  font-size: 22px;
+  color: #fff;
+  padding: 10px 10px 10px 10px;
+}
+
+.nav > .nav-btn {
+  display: none;
+}
+
+.nav > .nav-links {
+  display: inline;
+  float: right;
+  font-size: 18px;
+}
+
+.nav > .nav-links > a {
+  display: inline-block;
+  padding: 13px 10px 13px 10px;
+  text-decoration: none;
+  color: #efefef;
+}
+
+.nav > .nav-links > a:hover {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+.nav > #nav-check {
+  display: none;
+}
+
+@media (max-width:600px) {
+  .nav > .nav-btn {
+    display: inline-block;
+    position: absolute;
+    right: 0px;
+    top: 0px;
   }
-}
-@media (max-width: 767px){
-  div.searchbar {
-    max-width:200px;
+  .nav > .nav-btn > label {
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    padding: 13px;
   }
-}
-@media (max-width: 767px){
-  button.btn.btn-primary {
-    margin-right:10px;
+  .nav > .nav-btn > label:hover {
+    background-color: rgba(0, 0, 0, 0.3);
   }
-}
-@media (max-width: 767px){
-  div.button.login {
-    margin-right:150px;
+  .nav > .nav-btn > label > span {
+    display: block;
+    width: 25px;
+    height: 10px;
+    border-top: 2px solid #eee;
   }
-}
-<style>
-       body,html{
-    height: 100%;
+  .nav > .nav-links {
+    position: absolute;
+    display: block;
     width: 100%;
-    margin: 0;
-    padding: 0;
-    background: #e74c3c !important;
-    }
-
-    .searchbar{
-    margin-bottom: auto;
-    margin-top: auto;
-    height: 60px;
-    background-color: #353b48;
-    border-radius: 30px;
-    padding: 10px;
-    margin-right:-1000px;
-    }
-
-    .search_input{
-    color: white;
-    border: 0;
-    outline: 0;
-    background: none;
-    width: 0;
-    caret-color:transparent;
-    line-height: 40px;
-    transition: width 0.4s linear;
-    }
-
-    .searchbar:hover > .search_input{
-    padding: 0 10px;
-    width: 200px;
-    caret-color:red;
-    transition: width 0.4s linear;
-    }
-
-    .searchbar:hover > .search_icon{
-    background: white;
-    color: #e74c3c;
-    }
-
-    .search_icon{
-    height: 40px;
-    width: 40px;
-    float: right;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    color:white;
-    }
-    </style>
+    background-color: #333;
+    height: 0px;
+    transition: all 0.3s ease-in;
+    overflow-y: hidden;
+    top: 50px;
+    left: 0px;
+  }
+  .nav > .nav-links > a {
+    display: block;
+    width: 100%;
+  }
+  .nav > #nav-check:not(:checked) + .nav-links {
+    height: 0px;
+  }
+  .nav > #nav-check:checked + .nav-links {
+    height: calc(100vh - 50px);
+    overflow-y: auto;
+  }
+}
 </style>
 
 
