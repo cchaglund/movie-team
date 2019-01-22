9818002282d7d1c2e24a946c119fc3272bdc7e05
Sargon <Sargonpoli@hotmail.com>
2019-01-22 10:53:13 +0100

    Salongerna 1 & 2.

--- src/components/salong.vue
+++ src/components/salong.vue
@@ -1,25 +1,59 @@
 <template>
 	<div class="salong">
-<div class="card" style="width: 18rem;">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo6tmGUcajpJTWMM8zO7kRw2kgn5zSwcLX4Ly1XWPMgNFPOuhS" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="card" style="width: 100vw;">
+      <br>
+      <h1 class="card-title">Salong 1</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo6tmGUcajpJTWMM8zO7kRw2kgn5zSwcLX4Ly1XWPMgNFPOuhS" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h3 class="card-title">Allmän Information</h3>
+        <hr>
+        <p class="card-text">Ljudfilmen hade introducerats i USA i slutet av 1920-talet. Efter en viss skepsis kom nyheten snabbt igång i Sverige och 1930 producerades tio spelfilmer, varav redan sex var ljudfilmer. Den första svenskproducerade ljudfilmen premiärvisades den 10 augusti 1930 på Palladium. Den hette För hennes skull (1930) och hade Gösta Ekman d.ä. i huvudrollen. Nu gällde det att snabbt bygga om Stockholms biografer till ljudfilm. Först ut var Palladium och Skandia-Teatern.</p>
+        <br>
+        <hr>
+        <br>
+        <h2>Specifik information</h2>
+        <p>Ljudfilmen hade introducerats i USA i slutet av 1920-talet. Efter en viss skepsis kom nyheten snabbt igång i Sverige och 1930 producerades tio spelfilmer, varav redan sex var ljudfilmer. Den första svenskproducerade ljudfilmen premiärvisades den 10 augusti 1930 på Palladium. Den hette För hennes skull (1930) och hade Gösta Ekman d.ä. i huvudrollen. Nu gällde det att snabbt bygga om Stockholms biografer till ljudfilm. Först ut var Palladium och Skandia-Teatern.</p>
+        <br>
+        <hr>
+        <br>
+        <h2>Regler</h2>
+        <p class="card-text">Du har rätt att säga nej till direktmarknadsföring, inklusive analyser som görs i direktmarknadsföringssyfte.
+        Du kan välja bort direktmarknadsföring genom att följa instruktionerna i varje marknadsföringsutskick. Om du är medlem i vårt lojalitetsprogram kan du också göra detta på dina medlemsidor under uppdatera medlemsprofil..</p>
+      </div>
+    </div>
+    <div class="card" style="width: 100vw;">
+      <br>
+      <h1 class="card-title">Salong 2</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo6tmGUcajpJTWMM8zO7kRw2kgn5zSwcLX4Ly1XWPMgNFPOuhS" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h3 class="card-title">Allmän Information</h3>
+        <p class="card-text">Ljudfilmen hade introducerats i USA i slutet av 1920-talet. Efter en viss skepsis kom nyheten snabbt igång i Sverige och 1930 producerades tio spelfilmer, varav redan sex var ljudfilmer. Den första svenskproducerade ljudfilmen premiärvisades den 10 augusti 1930 på Palladium. Den hette För hennes skull (1930) och hade Gösta Ekman d.ä. i huvudrollen. Nu gällde det att snabbt bygga om Stockholms biografer till ljudfilm. Först ut var Palladium och Skandia-Teatern.</p>
+        <br>
+        <hr>
+        <br>
+        <h2>Specifik information</h2>
+        <p>Ljudfilmen hade introducerats i USA i slutet av 1920-talet. Efter en viss skepsis kom nyheten snabbt igång i Sverige och 1930 producerades tio spelfilmer, varav redan sex var ljudfilmer. Den första svenskproducerade ljudfilmen premiärvisades den 10 augusti 1930 på Palladium. Den hette För hennes skull (1930) och hade Gösta Ekman d.ä. i huvudrollen. Nu gällde det att snabbt bygga om Stockholms biografer till ljudfilm. Först ut var Palladium och Skandia-Teatern.</p>
+        <br>
+        <hr>
+        <br>
+        <h2>Regler</h2>
+        <p class="card-text">Du har rätt att säga nej till direktmarknadsföring, inklusive analyser som görs i direktmarknadsföringssyfte.
+        Du kan välja bort direktmarknadsföring genom att följa instruktionerna i varje marknadsföringsutskick. Om du är medlem i vårt lojalitetsprogram kan du också göra detta på dina medlemsidor under uppdatera medlemsprofil..</p>
+      </div>
+    </div>
   </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo6tmGUcajpJTWMM8zO7kRw2kgn5zSwcLX4Ly1XWPMgNFPOuhS" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-</div>
-
 </template>
+<style>
+  .card{
+    text-align: center;
+    margin: auto;
+    margin-top: 30px;
+    padding: 30px;
+  }
+</style>
 
 <script>
   export default {
-    name: salong
+    name: 'Salong'
   }
 </script>
--- src/views/Salonger.vue
+++ src/views/Salonger.vue
@@ -12,7 +12,7 @@ export default {
   props: {
     msg: String
   },
-  component: {
+  components: {
   	Salong
   }
 }
