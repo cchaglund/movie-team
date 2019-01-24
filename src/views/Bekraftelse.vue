d1a7ba6ccef188a3608713bc33ed5829afa6cb3f
Philip Angelin <philip_angelin@hotmail.com>
2019-01-24 10:09:39 +0100

    Bekraftelse.vue
    Nästan klar - redo för demo

--- src/views/Bekraftelse.vue
+++ src/views/Bekraftelse.vue
@@ -1,21 +1,25 @@
 <template>
     <div id="bekraftelse">
 
-        <h1>Tack för din order!</h1>
+        <h1>Tack för din beställning!</h1>
 
         <div class="container" id="container">
             <div class="row justify-content-center">
-                <div class="col">
-                    <p>Film:</p>
-                    <p>Plats:</p>
-                    <p>Biljetter:</p>
+                <div class="col" id="container-left">
+                    <p>Titel:</p>
                     <p>Tid:</p>
+                    <p>Datum:</p>
+                    <p>Pris:</p>
+                    <p>Platser:</p>
+                    <p>Biljetter:</p>
                 </div>
-                <div class="col">
-                    <p>Robin Hood</p>
-                    <p>Salong 2</p>
-                    <p>1 vuxen, 2 barn</p>
-                    <p>19.30</p>
+                <div class="col" id="container-right">
+                    <p>{{ title }}</p>
+                    <p>{{ time }}</p>
+                    <p>{{ date }}</p>
+                    <p>{{ price }}kr</p>
+                    <p>Rad: {{ seats.row }}, plats: {{ seats.seats }}</p>
+                    <p>Vuxen: {{ guests.adults }}, pensionär: {{ guests.pensioners }}, barn: {{ guests.children }} </p>
                 </div>
             </div>
         </div>
@@ -24,24 +28,42 @@
 
 <script>
 	
-import Footer from '@/components/Footer.vue'
 export default {
-  name: 'salonger',
+  name: 'bekraftelse',
   props: {
-    msg: String
   },
-  components: {
-  	Footer
+  data() {
+      return {
+          title: '',
+          time: '',
+          date: '',
+          price: '',
+          seats: {},
+          guests: {}
+      }
+  },
+  created() {
+      this.title = this.$route.params.title;
+      this.time = this.$route.params.time;
+      this.date = this.$route.params.date;
+      this.price = this.$route.params.price;
+      this.seats = this.$route.params.seats;
+      this.guests = this.$route.params.guests;
   }
+  /*components: {
+  	
+  }*/
 }
 
-$( document ).ready(function() {
-    $('<p>Hej</p>').appendTo('#container');
-})
+//jQuery - funkar inte
+/*$(document).ready(function() {
+    $('<p>container-left</p>').appendTo('#container-left');
+    $('<p>container-right</p>').appendTo('#container-right');
+})*/
 
 </script>
 
-<style>
+<style scoped>
 
 .container {
   position: absolute;
@@ -52,9 +74,21 @@ $( document ).ready(function() {
   background-color: rgb(21, 15, 16);
 }
 
+h1 {
+    position: absolute;
+    margin: 0;
+    top: 16%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    font-size: 50px;
+}
+
 .container p {
     margin-bottom: 0;
     margin: 5px;
+    font-size: 40px;
+    color: white;
 }
 
 </style>
