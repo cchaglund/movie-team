8ea9c5c03e531de85f45e47c64db6940fff93a08
Lavinia <lavinia.olsson@medieinstitutet.se>
2019-01-24 21:16:05 +0100

    Ändrat bakgrundsfärg och lite smått

--- src/views/Salonger.vue
+++ src/views/Salonger.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="salonger">
     <div class="stylealong">
-    <div class="card" style="width: 100vw;">
+    <div class="card" style="width: 100%;">
       <br>
       <h1 class="card-title">Salong 1</h1>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo6tmGUcajpJTWMM8zO7kRw2kgn5zSwcLX4Ly1XWPMgNFPOuhS" class="card-img-top" alt="...">
+      <img src="https://i1.wp.com/media.xn--kersbergafilmstudio-zwb.se/2015/12/biosalong.jpg?fit=1180%2C775" class="card-img-top" alt="...">
       <div class="card-body">
         <h3 class="card-title">Allmän Information</h3>
         <hr>
@@ -22,10 +22,10 @@
         Du kan välja bort direktmarknadsföring genom att följa instruktionerna i varje marknadsföringsutskick. Om du är medlem i vårt lojalitetsprogram kan du också göra detta på dina medlemsidor under uppdatera medlemsprofil..</p>
       </div>
     </div>
-    <div class="card" style="width: 100vw;">
+    <div class="card" style="width: 100%;">
       <br>
       <h1 class="card-title">Salong 2</h1>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo6tmGUcajpJTWMM8zO7kRw2kgn5zSwcLX4Ly1XWPMgNFPOuhS" class="card-img-top" alt="...">
+      <img src="https://i1.wp.com/media.xn--kersbergafilmstudio-zwb.se/2015/12/biosalong.jpg?fit=1180%2C775" class="card-img-top" alt="...">
       <div class="card-body">
         <h3 class="card-title">Allmän Information</h3>
         <p class="card-text">Ljudfilmen hade introducerats i USA i slutet av 1920-talet. Efter en viss skepsis kom nyheten snabbt igång i Sverige och 1930 producerades tio spelfilmer, varav redan sex var ljudfilmer. Den första svenskproducerade ljudfilmen premiärvisades den 10 augusti 1930 på Palladium. Den hette För hennes skull (1930) och hade Gösta Ekman d.ä. i huvudrollen. Nu gällde det att snabbt bygga om Stockholms biografer till ljudfilm. Först ut var Palladium och Skandia-Teatern.</p>
@@ -66,10 +66,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .card{
-    text-align: center;
-    margin: auto;
-    
-    padding: 30px;
-    color:black;
+    text-align: left;
+    margin: auto;    
+    padding: 5%;
+    color: white;    
+    align-items: center;
+    background-color: #6f2232;
   }
 </style>
