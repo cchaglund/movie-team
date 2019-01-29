45f631c72a0b1012c58d0a44be96babd17b6673b
Christofer Haglund <christofer.haglund@outlook.com>
2019-01-29 12:00:38 +0100

    användarsidan

--- src/views/Anvandare.vue
+++ src/views/Anvandare.vue
@@ -1,3 +1,14 @@
 <template>
+	<Bokningsbekraftelse />
 	
 </template>
+
+<script>
+	import Bokningsbekraftelse from '@/components/Bokningsbekraftelse.vue'
+	export default {
+		name: 'anvandare',
+		components: {
+			Bokningsbekraftelse
+		}
+	}
+</script>
